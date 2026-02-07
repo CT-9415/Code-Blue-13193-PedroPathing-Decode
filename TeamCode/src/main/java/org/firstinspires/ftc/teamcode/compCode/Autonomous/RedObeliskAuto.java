a393a20b4b9965cb44411086818320c1dc568ba6
CT-9415 <nicholas.pechotta28@d155.org>
2026-02-07 13:26:46 -0600

    auto stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/compCode/Autonomous/RedObeliskAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/compCode/Autonomous/RedObeliskAuto.java
@@ -15,11 +15,12 @@ import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 import java.util.Timer;
 
 @Autonomous
-public class RedObeliskAuto extends OpMode{
+public class RedObeliskAuto extends OpMode {
     private TelemetryManager panelsTelemetry; // Panels Telemetry instance
     public Follower follower; // Pedro Pathing follower instance
     private int pathState; // Current autonomous path state (state machine)
     private Paths paths; // Paths defined in the Paths class
+    private Timer pathTimer, actionTimer, opmodeTimer;
 
     @Override
     public void init() {
@@ -73,4 +74,53 @@ public class RedObeliskAuto extends OpMode{
 
                     .build();
         }
+
+
+    }
+    public void autonomousPathUpdate() {
+        switch (pathState) {
+            case 0:
+                follower.followPath(scorePreload);
+                setPathState(1);
+                break;
+            case 1:
+            /* You could check for
+            - Follower State: "if(!follower.isBusy()) {}"
+            - Time: "if(pathTimer.getElapsedTimeSeconds() > 1) {}"
+            - Robot Position: "if(follower.getPose().getX() > 36) {}"
+            */
+                /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the scorePose's position */
+                if (!follower.isBusy()) {
+                    /* Score Preload */
+                    /* Since this is a pathChain, we can have Pedro hold the end point while we are grabbing the sample */
+                    follower.followPath(grabPickup1, true);
+                    setPathState(2);
+                }
+                break;
+            case 2:
+                /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the pickup1Pose's position */
+                if (!follower.isBusy()) {
+                    /* Grab Sample */
+                    /* Since this is a pathChain, we can have Pedro hold the end point while we are scoring the sample */
+                    follower.followPath(scorePickup1, true);
+                    setPathState(3);
+                }
+                break;
+            case 3:
+                /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the scorePose's position */
+                if (!follower.isBusy()) {
+                    /* Score Sample */
+                    /* Since this is a pathChain, we can have Pedro hold the end point while we are grabbing the sample */
+                    follower.followPath(grabPickup2, true);
+                    setPathState(4);
+                }
+                break;
+
+        }
+    }
+            public void setPathState(int pState) {
+                pathState = pState;
+                pathTimer.resetTimer();
+            }
+
 }
